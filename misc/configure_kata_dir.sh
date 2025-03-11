948ce23714d6d02ab36eb8710b6bfea77bbebe0a
Claudio Carvalho <cclaudio@linux.ibm.com>
2025-03-11 11:18:20 -0400

    Fix /opt/kata symlink

--- misc/configure_kata_dir.sh
+++ misc/configure_kata_dir.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-KATA_DIR=${1:-/opt/kata}
+KATA_DIR=${1}
 
-read -p "Configure ${KATA_DIR}[y/N]?" confirm && [[ $confirm == [yY] ]] || exit 1
+[ -d ${KATA_DIR} ] || { echo "${KATA_DIR} not valid"; exit 1; }
+read -p "Configure ${KATA_DIR} [y/N]?" confirm && [[ $confirm == [yY] ]] || exit 1
 
-sudo rm /opt/kata || exit 1
-sudo ln -s ${KATA_DIR} kata || exit 1
+echo "Fix /opt/kata symlink"
+[ -f /opt/kata ] && sudo rm /opt/kata
+sudo ln -s ${KATA_DIR} /opt/kata || exit 1
 
 echo "Configuring ${KATA_DIR} selinux attributes"
 pushd ${KATA_DIR}
